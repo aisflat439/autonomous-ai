e13349f0bab9f8a70ad7983ef543660f16713e3d
Devin <fitzsimonsdevin@gmail.com>
2025-06-13 10:32:58 -0600

    chore: add tanstack router

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -1,9 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  // @ts-expect-error - incorrect vite version installed, TODO: fix this
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    // @ts-expect-error - incorrect vite version installed, TODO: fix this
+    tanstackRouter({
+      target: "react",
+      autoCodeSplitting: true,
+      verboseFileRoutes: false,
+    }),
+    // @ts-expect-error - incorrect vite version installed, TODO: fix this
+    react(),
+    // @ts-expect-error - incorrect vite version installed, TODO: fix this
+    tailwindcss(),
+  ],
 });
